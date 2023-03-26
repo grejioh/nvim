625aed9ffe1e0c1036158e94c1e21ce8ce318479
grejioh <grejioh@outlook.com>
2023-03-26 14:27:13 +0800

    adjust some tab things

--- init.lua
+++ init.lua
@@ -495,3 +495,9 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- my default tabwidth
+local TABWIDTH = 4
+vim.opt.tabstop = TABWIDTH
+vim.opt.shiftwidth = TABWIDTH
+vim.opt.expandtab = true
